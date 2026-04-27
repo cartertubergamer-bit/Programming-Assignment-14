b9cc369edf9f4915028d2467dc75746a0cb998d9
Carter Cox <cartertubergamer@gmail.com>
2026-04-26 21:39:53 -0400

    Vehicle .h and .cpp file contents added

--- CIS 1202 - Inheritance, Polymorphism, and Virtual Functions/Vehicle.cpp
+++ CIS 1202 - Inheritance, Polymorphism, and Virtual Functions/Vehicle.cpp
@@ -5,3 +5,37 @@
 //  Created by Carter Cox on 4/26/26.
 //
 
+#include "Vehicle.h"
+#include <iostream>
+using namespace std;
+
+Vehicle::Vehicle() {
+    manufacturer = "";
+    yearBuilt = 0;
+}
+
+Vehicle::Vehicle(string manufacturer, int yearBuilt) {
+    this->manufacturer = manufacturer;
+    this->yearBuilt = yearBuilt;
+}
+
+string Vehicle::getManufacturer() const {
+    return manufacturer;
+}
+
+int Vehicle::getYearBuilt() const {
+    return yearBuilt;
+}
+
+void Vehicle::setManufacturer(string manufacturer) {
+    this->manufacturer = manufacturer;
+}
+
+void Vehicle::setYearBuilt(int yearBuilt) {
+    this->yearBuilt = yearBuilt;
+}
+
+void Vehicle::displayInfo() const {
+    cout << "Manufacturer: " << manufacturer << endl;
+    cout << "Year Built: " << yearBuilt << endl;
+}
--- CIS 1202 - Inheritance, Polymorphism, and Virtual Functions/Vehicle.h
+++ CIS 1202 - Inheritance, Polymorphism, and Virtual Functions/Vehicle.h
@@ -5,3 +5,28 @@
 //  Created by Carter Cox on 4/26/26.
 //
 
+#ifndef VEHICLE_H
+#define VEHICLE_H
+
+#include <string>
+using namespace std;
+
+class Vehicle {
+private:
+    string manufacturer;
+    int yearBuilt;
+
+public:
+    Vehicle();
+    Vehicle(string manufacturer, int yearBuilt);
+
+    string getManufacturer() const;
+    int getYearBuilt() const;
+
+    void setManufacturer(string manufacturer);
+    void setYearBuilt(int yearBuilt);
+
+    void displayInfo() const;
+};
+
+#endif
