34834cfa9f20152e44beebf8ec3f5c3aa4b495be
Carter Cox <cartertubergamer@gmail.com>
2026-04-26 21:55:45 -0400

    main cpp file contents added

--- CIS 1202 - Inheritance, Polymorphism, and Virtual Functions/main.cpp
+++ CIS 1202 - Inheritance, Polymorphism, and Virtual Functions/main.cpp
@@ -2,4 +2,55 @@
 // CIS 1202 - Inheritance, Polymorphism, and Virtual Functions
 // 4/26/26
 
+#include <iostream>
+#include "Vehicle.h"
+#include "Car.h"
+#include "Truck.h"
 
+using namespace std;
+
+int main() {
+
+    string manufacturer;
+    int yearBuilt;
+
+    // VEHICLE
+    cout << "Enter Vehicle Manufacturer: ";
+    getline(cin, manufacturer);
+    cout << "Enter Year Built: ";
+    cin >> yearBuilt;
+    cin.ignore();
+
+    Vehicle v(manufacturer, yearBuilt);
+    cout << "\n--- Vehicle Info ---\n";
+    v.displayInfo();
+
+    // CAR
+    int doors;
+    cout << "\nEnter Car Manufacturer: ";
+    getline(cin, manufacturer);
+    cout << "Enter Year Built: ";
+    cin >> yearBuilt;
+    cout << "Enter Number of Doors: ";
+    cin >> doors;
+    cin.ignore();
+
+    Car c(manufacturer, yearBuilt, doors);
+    cout << "\n--- Car Info ---\n";
+    c.displayInfo();
+
+    // TRUCK
+    double towing;
+    cout << "\nEnter Truck Manufacturer: ";
+    getline(cin, manufacturer);
+    cout << "Enter Year Built: ";
+    cin >> yearBuilt;
+    cout << " Enter Towing Capacity: ";
+    cin >> towing;
+
+    Truck t(manufacturer, yearBuilt, towing);
+    cout << "\n--- Truck Info ---/n";
+    t.displayInfo();
+
+    return 0;
+}
