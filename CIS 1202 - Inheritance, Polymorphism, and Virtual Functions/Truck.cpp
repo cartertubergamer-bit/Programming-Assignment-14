26d6d927984c6f9fea26ee255f20e3f6b5df4739
Carter Cox <cartertubergamer@gmail.com>
2026-04-26 21:50:41 -0400

    truck .h and .cpp contents added

--- CIS 1202 - Inheritance, Polymorphism, and Virtual Functions/Truck.cpp
+++ CIS 1202 - Inheritance, Polymorphism, and Virtual Functions/Truck.cpp
@@ -5,3 +5,28 @@
 //  Created by Carter Cox on 4/26/26.
 //
 
+#include "Truck.h"
+#include <iostream>
+using namespace std;
+
+Truck::Truck() : Vehicle() {
+    towingCapacity = 0.0;
+}
+
+Truck::Truck(string manufacturer, int yearBuilt, double towingCapacity)
+    : Vehicle(manufacturer, yearBuilt) {
+    this->towingCapacity = towingCapacity;
+}
+
+double Truck::getTowingCapacity() const {
+    return towingCapacity;
+}
+
+void Truck::setTowingCapacity(double towingCapacity) {
+    this->towingCapacity = towingCapacity;
+}
+
+void Truck::displayInfo() const {
+    Vehicle::displayInfo();
+    cout << "Towing Capacity: " << towingCapacity << endl;
+}
--- CIS 1202 - Inheritance, Polymorphism, and Virtual Functions/Truck.h
+++ CIS 1202 - Inheritance, Polymorphism, and Virtual Functions/Truck.h
@@ -5,3 +5,23 @@
 //  Created by Carter Cox on 4/26/26.
 //
 
+#ifndef TRUCK_H
+#define TRUCK_H
+
+#include "Vehicle.h"
+
+class Truck : public Vehicle {
+private:
+    double towingCapacity;
+
+public:
+    Truck();
+    Truck(string manufacturer, int yearBuilt, double towingCapacity);
+
+    double getTowingCapacity() const;
+    void setTowingCapacity(double towingCapacity);
+
+    void displayInfo() const;
+};
+
+#endif
