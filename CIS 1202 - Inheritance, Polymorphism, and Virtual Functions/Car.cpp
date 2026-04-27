c4091c99805ee13632e7fb274f08b1d6f772fc75
Carter Cox <cartertubergamer@gmail.com>
2026-04-26 21:45:01 -0400

    car .h and .cpp contents added

--- CIS 1202 - Inheritance, Polymorphism, and Virtual Functions/Car.cpp
+++ CIS 1202 - Inheritance, Polymorphism, and Virtual Functions/Car.cpp
@@ -5,3 +5,28 @@
 //  Created by Carter Cox on 4/26/26.
 //
 
+#include "Car.h"
+#include <iostream>
+using namespace std;
+
+Car::Car() : Vehicle() {
+    numDoors = 0;
+}
+
+Car::Car(string manufacturer, int yearBuilt, int numDoors)
+    : Vehicle(manufacturer, yearBuilt) {
+    this->numDoors = numDoors;
+}
+
+int Car::getNumDoors() const {
+    return numDoors;
+}
+
+void Car::setNumDoors(int numDoors) {
+    this->numDoors = numDoors;
+}
+
+void Car::displayInfo() const {
+    Vehicle::displayInfo();
+    cout << "Number of Doors: " << numDoors << endl;
+}
--- CIS 1202 - Inheritance, Polymorphism, and Virtual Functions/Car.h
+++ CIS 1202 - Inheritance, Polymorphism, and Virtual Functions/Car.h
@@ -5,3 +5,23 @@
 //  Created by Carter Cox on 4/26/26.
 //
 
+#ifndef CAR_H
+#define CAR_H
+
+#include "Vehicle.h"
+
+class Car : public Vehicle {
+private:
+    int numDoors;
+
+public:
+    Car();
+    Car(string manufacturer, int yearBuilt, int numDoors);
+
+    int getNumDoors() const;
+    void setNumDoors(int numDoors);
+
+    void displayInfo() const;
+};
+
+#endif
