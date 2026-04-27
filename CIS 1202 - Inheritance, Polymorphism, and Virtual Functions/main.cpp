f9691dd570d57dad1dca7f42b0709d62de1bf1ca
Carter Cox <cartertubergamer@gmail.com>
2026-04-26 21:22:50 -0400

    Initial Commit

---
+++ CIS 1202 - Inheritance, Polymorphism, and Virtual Functions/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CIS 1202 - Inheritance, Polymorphism, and Virtual Functions
+//
+//  Created by Carter Cox on 4/26/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
